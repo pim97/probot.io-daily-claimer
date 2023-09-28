dfb822e61359c409b6a8861b0b7a9fd57c858716
pim de bree <pim_debree@hotmail.com>
2023-09-28 10:11:11 +0200

    Fix new Probot updates

--- script.js
+++ script.js
@@ -1,64 +1,13 @@
 const Scrappey = require('scrappey-wrapper');
-const crypto = require('crypto');
-const CapSolver = require('node-capsolver')
 
 // Replace the following details with your own details
 const SCRAPPEY_API_KEY = 'scrappey.com';
-const AUTHORIZATION_HEADER_KEY = 'autoirization_header_key_from_probot.io_when_logging_in'
-const CAPSOLVER_API_KEY = 'capsolver.com'
-const PROXY = 'your_proxy_here'
-
-//Do not change
-const PROBOT_V2_KEY = '6LceDTMUAAAAAFQhYmUzmvKn6uahUjgguWpg-KO1'
+const AUTHORIZATION_HEADER_KEY = 'probot_auth_header'
 
 // Create an instance of Scrappey
 const scrappey = new Scrappey(SCRAPPEY_API_KEY);
-const handler = new CapSolver(CAPSOLVER_API_KEY, {
-    verbose: true, // Optional
-})
-const date = new Date().toISOString();
 
-/**
- * Extra security measure from Probot
- * @param {*} id 
- * @returns 
- */
-function hash(id) {
-    const e = {
-        id: id
-    }; 
-    const i = date; 
-    const concatenatedString = "Backlands-Magnetize2-Parkway".concat(null == e ? void 0 : e.id).concat(i);
-    const hash = crypto.createHash('sha256').update(concatenatedString).digest('hex');
-    return hash;
-}
-
-/**
- * Using CapSolver to solve the captcha
- * @returns 
- */
-async function getCaptchaAnswer() {
-    const taskData =
-    {
-        type: "ReCaptchaV2TaskProxyLess",
-        websiteURL: "https://probot.io/api/claim_daily",
-        websiteKey: PROBOT_V2_KEY
-    }
-    let response;
-
-    let b = await handler.getBalance();
-    if (b.balance > 0) {
-        response = await handler.solve({
-            type: taskData.type,
-            websiteURL: taskData.websiteURL,
-            websiteKey: taskData.websiteKey,
-            proxy: PROXY
-        })
-    } else {
-        console.log("Insufficient balance.")
-    }
-    return response.solution.gRecaptchaResponse
-}
+const PROXY = ''
 
 /**
  * Runs the script
@@ -66,7 +15,7 @@ async function getCaptchaAnswer() {
 async function run() {
     try {
         const sessionRequest = await scrappey.createSession({
-            proxy: PROXY
+            // proxy: PROXY //optional
         });
         const session = sessionRequest.session;
 
@@ -81,28 +30,18 @@ async function run() {
         })
 
         const result = JSON.parse(get.solution.innerText)
-    
-        const captcha = await getCaptchaAnswer()
-
-        console.log(`Found answer answer ${captcha}`)
-
-        const post = {
-            c: hash(result.id),
-            captcha: captcha,
-            d: date,
-        }
 
         const claim = await scrappey.post({
             url: 'https://probot.io/api/claim_daily',
             session: session,
             customHeaders: {
                 "content-type": "application/json",
-                "authorization": AUTHORIZATION_HEADER_KEY,
-                "referer": "https://probot.io/daily",
-                "origin": "https://probot.io",
+                "authorization": AUTHORIZATION_HEADER_KEY
             },
-            requestType: "request",
-            postData: JSON.stringify(post),
+            probotData: {
+                id: result.id,
+            },
+            postData: "probot",
         })
 
         console.log(claim)
